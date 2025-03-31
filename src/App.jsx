aa0f02617b1b4b686812cf2d4bd565dff1243fa8
Darien Bernal <darien.bernal@gmail.com>
2025-03-31 13:34:11 -0400

    Setup Header and Card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Header from './components/Header'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <Card title="Projects" />
+      <Card title="About Me" />
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card(props) {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+        </div>
+    )
+}
+export default Card
